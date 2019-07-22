fddab24a6eea56a8b957529c1ff53d89c3a0eef0
Keith C. Ivey <keith@iveys.org>
2019-07-22 17:21:19 -0400

    fix query in make-html.js for new database structure

--- make-html.js
+++ make-html.js
@@ -23,19 +23,21 @@ async function main() {
 
 function getData() {
     return db.raw(
-        `select
-            m1.name as name1,
-            m2.name as name2,
-            sum(case when mv1.result=mv2.result then 1 else 0 end) as same,
-            count(*) as total
-        from member_votes mv1, member_votes mv2, members m1, members m2, votes v
-        where mv1.vote_id=mv2.vote_id
-            and m1.id=mv1.member_id and m2.id=mv2.member_id
-            and v.id=mv1.vote_id
-            and v.noes>0
-            and mv1.result in (1,2)
-            and mv2.result in (1,2)
-        group by 1,2`
+        `SELECT
+            m1.name AS name1,
+            m2.name AS name2,
+            SUM(CASE WHEN mv1.result = mv2.result THEN 1 ELSE 0 END) AS same,
+            COUnt(*) AS total
+        FROM member_votes mv1, member_votes mv2, members m1, members m2, votes v
+        WHERE mv1.council_period = mv2.council_period AND mv1.vote_id = mv2.vote_id
+            AND mv1.council_period = m1.council_period AND m1.id = mv1.member_id
+            AND mv2.council_period = m2.council_period AND m2.id = mv2.member_id
+            AND v.council_period = mv1.council_period AND v.id = mv1.vote_id
+            AND v.noes > 0
+            AND mv1.result IN (1, 2) AND mv2.result IN (1, 2)
+            AND mv1.council_period >= ?
+        GROUP BY 1, 2`,
+        [22]
     );
 }
 
